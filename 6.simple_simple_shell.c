5ed0c9cea45bb0b4eccb7e36f0817142c46eaf9b
Grace Fallon <graceleefallon@gmail.com>
2021-04-08 19:46:17 +0000

    Move tokenizing into its own function

--- 6.simple_simple_shell.c
+++ 6.simple_simple_shell.c
@@ -7,42 +7,25 @@
 extern char **environ; /* enviornmental array */
 int _strlen(char *s); /* string length helper function */
 void free_token(char *array[], int size); /* data free-ing helper function */
-
+char **tokenize(char *line, int *token_count);
 
 int main(void)
 {
-        int i = 0, status;
+        int  token_count = 0, status;
         char *lineptr = NULL;
-        char *token = NULL;
-	char **exec_str = calloc(50, sizeof(char *));
+	char **exec_str = NULL; 
         size_t n = 0;
         ssize_t character;
         pid_t child;
 
         printf("$ "); /* prompt user for input */
         getline(&lineptr, &n, stdin);
-        token = strtok(lineptr, " ");
-        while (token != NULL)
-        {
-        /*fill tokens into array of strings: exec_str*/
-                exec_str[i] = malloc(sizeof(token));
-                if (exec_str[i] == NULL) /* malloc fail */
-                {
-                        free(lineptr);
-                        free_token(exec_str, i);
-			exit(-1);
-            	}
-                strcpy(exec_str[i], token);
-                token = strtok(NULL, " ");
-                i++;
-        }
-	/* replace newline character with null byte */
-	exec_str[i - 1][_strlen(exec_str[i - 1]) - 1] = '\0'; 
+	exec_str = tokenize(lineptr, &token_count);
         child = fork();
         if (child == -1) /* fork fail */
         {
                 free(lineptr);
-                free_token(exec_str, i);
+                free_token(exec_str, token_count);
                 exit(0);
         }
 	if (child == 0)
@@ -52,7 +35,7 @@ int main(void)
 		if (character == EOF) /* execve fail */
 		{
 			free(lineptr);
-			free_token(exec_str, i);
+			free_token(exec_str, token_count);
 			exit(0);
 		}
         }
@@ -61,8 +44,33 @@ int main(void)
 		wait(&status);
         }
         free(lineptr);
-        free_token(exec_str, i);
+        free_token(exec_str, token_count);
         return (0);
+}
+
+char **tokenize(char *line, int *token_count)
+{
+	char *token = NULL;
+	char **exec_str = malloc(50 * sizeof(char *));
+
+	*token_count = 0;
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		exec_str[*token_count] = malloc(sizeof(token));
+		if (exec_str[*token_count] == NULL)
+		{
+			free(line);
+			free_token(exec_str, *token_count);
+			exit (0);
+		}
+		strcpy(exec_str[*token_count], token);
+		token = strtok(NULL, " ");
+		*token_count = *token_count + 1;
+	}
+	exec_str[*token_count - 1][_strlen(exec_str[*token_count - 1]) - 1] = '\0';
+
+	return(exec_str);
 }
 
 /**
@@ -84,10 +92,8 @@ int _strlen(char *s)
         int x;
 
         x = 0;
-
-        while (*s != '\0')
+        while (s[x] != '\0')
         {
-                s++;
                 x++;
         }
 
