98788dba22b6ce5062f07c74073d4c5fc9997898
Grace Fallon <graceleefallon@gmail.com>
2021-04-08 20:11:36 +0000

    Initial commit for header file shell.h

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/* Core Functions */
+
+/* Helper Functions */
+
+/* Find a the length of a string */
+int _strlen(char *s);
+
+/* Determine whether two strings are identical */
+int _strcmp(char *s1, char *s2);
+
+
+/* SHELL_H end */
+#endif
