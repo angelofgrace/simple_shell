fc72ffbbac4f14a430a3fdf6f8e47cda058d12e5
Grace Fallon <graceleefallon@gmail.com>
2021-04-12 00:49:27 +0000

    Strtok x2 to dynamically allocate exec_str; valgrind solves

--- 6.simple_simple_shell.c
+++ 6.simple_simple_shell.c
@@ -6,97 +6,108 @@
 #include <sys/types.h>
 #include "shell.h"
 int _strlen(char *s); /* string length helper function */
-void free_token(char *array[], int size); /* data free-ing helper function */
-char **tokenize(char *line, int *token_count);
+char **tokenize(char *string, char *delim);
+void free_ptr_arr(char **dbl_ptr);
 
 int main(void)
 {
-        int  token_count = 0, status;
-        char *lineptr = NULL;
+	int  status;
+	char *lineptr = NULL;
 	char **exec_str = NULL; 
-        size_t n = 0;
-        ssize_t character;
-        pid_t child;
+	size_t n = 0;
+	ssize_t character;
+	pid_t child;
 
-	while(1)
+	while (getline(&lineptr, &n, stdin) != -1)
 	{
-        printf("$ "); /* prompt user for input */
-        getline(&lineptr, &n, stdin);
-	exec_str = tokenize(lineptr, &token_count);
-	
-	child = fork();
-        if (child == -1) /* fork fail */
-        {
-                free(lineptr);
-                free_token(exec_str, token_count);
-                exit(0);
-        }
-	if (child == 0)
-        {
-		/* execute bash script, overwriting child process */
-                character = execve(exec_str[0], exec_str, environ);
-		if (character == EOF) /* execve fail */
-		{
-			free(lineptr);
-			free_token(exec_str, token_count);
-			exit(0);
-		}
-        }
-        else
-        {
-		wait(&status);
-        }
-	}
-        free(lineptr);
-        free_token(exec_str, token_count);
+        	printf("$ "); /* prompt user for input */
+		lineptr[_strlen(lineptr) - 1] = '\0'; /* replace /n with null byte */
+		exec_str = tokenize(lineptr, " "); /* tokenize commands */
 
+		child = fork();
+	        if (child == -1) /* fork fail */
+        	{
+                	free(lineptr);
+	                free(exec_str);
+        	        exit(0);
+	        }
+		if (child == 0)
+	        {
+			/* execute shell script, overwriting child process */
+                	character = execve(exec_str[0], exec_str, environ);
+			if (character == EOF) /* execve fail */
+			{
+				free(lineptr);
+				free(exec_str);
+				exit(0);
+			}
+        	}
+	        else
+        	{
+			wait(&status);
+	        }
+	free(exec_str);
+	}
+	free(lineptr);
         return (0);
 }
 
-char **tokenize(char *line, int *token_count)
-{
-	char *token = NULL;
-	char **exec_str = malloc(50 * sizeof(char *));
 
-	*token_count = 0;
-	token = strtok(line, " ");
+char **tokenize(char *string, char *delim)
+{
+	int token_count = 1, i = 0, x = 0, len = 0;
+	char *token = NULL;
+	char **parsed_str = NULL;
+
+	len = _strlen(string);
+	token = strtok(string, delim); /* init strtok with default delim */
 	while (token != NULL)
 	{
-		exec_str[*token_count] = malloc(sizeof(token));
-		if (exec_str[*token_count] == NULL)
-		{
-			free(line);
-			free_token(exec_str, *token_count);
-			exit (0);
-		}
-		strcpy(exec_str[*token_count], token);
-		token = strtok(NULL, " ");
-		*token_count = *token_count + 1;
+		token = strtok(NULL, delim); /* iterate strtok */
+		token_count++; /* gauge number of required pointers */
 	}
-	exec_str[*token_count - 1][_strlen(exec_str[*token_count - 1]) - 1] = '\0';
-
-	return(exec_str);
+	for (x = 0; x < len; x++)
+	{
+		if (string[x] == '\0')
+			string[x] = delim[0];
+	}
+	/* allocate space for pointers to the array of strings */
+	parsed_str = malloc(sizeof(char *) * token_count);
+	if (parsed_str == NULL)
+	{
+		free(string);
+		exit(1);
+	}
+	token = strtok(string, delim); /* init strtok again */
+	parsed_str[token_count - 1] = NULL; /* NULL terminate the array of strings */
+	while (i <= token_count)
+	{
+		parsed_str[i] = (token);
+		token = strtok(NULL, delim);
+		token_count--;
+		i++;
+	}
+	return (parsed_str);
 }
 
-/**
-* free_token - frees tokens upon error
+/*
+* _strlen - determine the length of an array of characters
+* @s: input string literal
+*
+* Return: number of characters in the string
 */
-void free_token(char **exec_str, int i)
-{
-        while (i >= 0)
-        {
-                free(exec_str[i]);
-                i--;
-        }
-        free(exec_str);
-}
-
 
 int _strlen(char *s)
 {
         int x;
 
-        x = 0;
+	x = 0;
+
+	if (s == NULL)
+	{
+		return(x);
+ 	}
+
         while (s[x] != '\0')
         {
                 x++;
